fa9b18425b61ae91115e78b78cfce6dc45f6586d
Karim-308 <2001118@eng.asu.edu.eg>
2023-10-03 19:51:15 -0700

    100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
-for i in range(122, 96, -1):
-    if i % 2 == 0:  # Even ASCII value
-        print(chr(i - 32), end='')
-    else:
-        print(chr(i), end='')
+print(''.join(chr(i - 32) if i % 2 == 0 else chr(i) for i in range(122, 96, -1)))
+
