f8de859acc27fb390ddf404909cae3444ab63ffd
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2023-10-10 04:23:51 +0300

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python3
 def multiple_returns(sentence):
-    if not sentence:
-        return (0, None)
-    return (len(sentence), sentence[0])
-
+    if len(sentence) == 0:
+        my_tuple = (0, None)
+    else:
+        my_tuple = (len(sentence), sentence[:1])
+    return(my_tuple)
