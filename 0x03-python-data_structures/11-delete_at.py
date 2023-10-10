7d22e4f0d04f7ba4540c28de0db19c2ba092a7ce
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2023-10-10 04:35:53 +0300

    Update 11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-def divisible_by_2(my_list=[]):
-    new_list = []
-    for number in my_list:
-        if (number % 2) == 0:
-            new_list.append(True)
-        else:
-            new_list.append(False)
-    return(new_list)
+def delete_at(my_list=[], idx=0):
+    length_list = len(my_list) - 1
+    if idx < 0 or idx > length_list:
+        return(my_list)
+    else:
+        del my_list[idx]
+        return(my_list)
