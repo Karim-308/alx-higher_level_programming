1db832beef3d02d7523ca378276d14eecb316aa0
Karim-308 <2001118@eng.asu.edu.eg>
2023-10-09 17:57:58 -0700

    1 2 3 4 5 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for idx, val in enumerate(row):
+            print("{:d}".format(val), end=" " if idx != len(row) - 1 else "\n")
+
