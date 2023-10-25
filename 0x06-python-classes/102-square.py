4663bdabc53433fcc921ddea69e22820a614af82
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2023-10-25 03:17:27 +0300

    Update 102-square.py

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,53 +1,18 @@
 #!/usr/bin/python3
-class Square:
-    """Represents a Square."""
+Square = __import__('102-square').Square
 
-    def __init__(self, size=0):
-        """Initialize a new Square.
+s_5 = Square(5)
+s_6 = Square(6)
 
-        Args:
-            size (float or int): Size of the side of the Square.
-        """
-        self.size = size
-
-    @property
-    def size(self):
-        """Get the current size of the square."""
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        """Set the size of the square."""
-        if not isinstance(value, (int, float)):
-            raise TypeError("size must be a number")
-        if value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
-
-    def area(self):
-        """Return the current square area."""
-        return self.__size ** 2
-
-    def __eq__(self, other):
-        """Return True if both squares have the same area, False otherwise."""
-        return self.area() == other.area()
-
-    def __ne__(self, other):
-        """Return True if squares have different areas, False otherwise."""
-        return self.area() != other.area()
-
-    def __lt__(self, other):
-        """Return True if the current square has a smaller area than the other, False otherwise."""
-        return self.area() < other.area()
-
-    def __le__(self, other):
-        """Return True if the current square has a smaller or equal area than the other, False otherwise."""
-        return self.area() <= other.area()
-
-    def __gt__(self, other):
-        """Return True if the current square has a bigger area than the other, False otherwise."""
-        return self.area() > other.area()
-
-    def __ge__(self, other):
-        """Return True if the current square has a bigger or equal area than the other, False otherwise."""
-        return self.area() >= other.area()
+if s_5 < s_6:
+    print("Square 5 < Square 6")
+if s_5 <= s_6:
+    print("Square 5 <= Square 6")
+if s_5 == s_6:
+    print("Square 5 == Square 6")
+if s_5 != s_6:
+    print("Square 5 != Square 6")
+if s_5 > s_6:
+    print("Square 5 > Square 6")
+if s_5 >= s_6:
+    print("Square 5 >= Square 6")
