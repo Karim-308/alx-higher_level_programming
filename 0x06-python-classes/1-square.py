0b01e11060de93a242328f6d21a397e6fcfab311
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2023-10-25 03:15:42 +0300

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
-class Square:
-    """Represents a Square"""
+"""Defines a class Square"""
 
+
+class Square:
+    """
+    Class that defines properties of square by: (based on 0-square.py).
+
+    Attributes:
+        size: size of a square (1 side).
+    """
     def __init__(self, size):
-        """Initialize a new Square with a given size
+        """Creates new instances of square (1 side).
+
         Args:
-            size (int): Size of the side of the Square
+            size: size of the square.
         """
         self.__size = size
