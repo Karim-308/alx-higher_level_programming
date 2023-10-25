a2bbf58bd2a5808b9bacde4a446199cece00b578
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2023-10-25 03:03:13 +0300

    Create 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+class Square:
+    """Represents a Square."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square with a given size.
+
+        Args:
+            size (int): Size of the side of the Square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
