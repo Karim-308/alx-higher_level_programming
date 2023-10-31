1831040aa4c68858b6874dd840a6340b31e8f788
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2023-10-31 03:02:28 +0200

    Update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,53 +1,37 @@
 #!/usr/bin/python3
 import sys
 
-def is_safe(board, row, col, n):
-    # Check this row on the left
-    for i in range(col):
-        if board[row][i] == 1:
+def is_valid(queens, col):
+    row = len(queens)
+    for q_row, q_col in enumerate(queens):
+        if q_col == col or \
+           q_col - q_row == col - row or \
+           q_col + q_row == col + row:
             return False
-
-    # Check upper diagonal on the left
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    # Check lower diagonal on the left
-    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
     return True
 
-def solve_nqueens(board, col, n, solutions):
-    if col >= n:
-        solutions.append([i for i, row in enumerate(board) for j, cell in enumerate(row) if cell])
+def n_queens(n, board=[]):
+    if len(board) == n:
+        print([[i, board[i]] for i in range(n)])
         return
 
-    for i in range(n):
-        if is_safe(board, i, col, n):
-            board[i][col] = 1
-            solve_nqueens(board, col + 1, n, solutions)
-            board[i][col] = 0  # backtrack
+    for col in range(n):
+        if is_valid(board, col):
+            n_queens(n, board + [col])
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    
+
     try:
         n = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
-    
+
     if n < 4:
         print("N must be at least 4")
         sys.exit(1)
 
-    board = [[0] * n for _ in range(n)]
-    solutions = []
-    solve_nqueens(board, 0, n, solutions)
-
-    for solution in solutions:
-        print(solution)
+    n_queens(n)
