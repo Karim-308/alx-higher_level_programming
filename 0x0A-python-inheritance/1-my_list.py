e2749723ee89a6dc32be05780b3a9d14128330ad
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2023-11-07 02:29:26 +0200

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-"""
-===========================
-Module with class MyList
-===========================
-"""
+"""Module for the MyList class."""
 
 
 class MyList(list):
-    """Class with method print_sorted"""
-    pass
-
+    """A class that inherits from the list class and adds a print_sorted method."""
     def print_sorted(self):
-        """Methot that sorted a list"""
+        """Print the list sorted in ascending order."""
+        print(sorted(self))
 
-        print(sorted(list(self)))
+
+if __name__ == "__main__":
+    MyList = __import__('1-my_list').MyList
+
+    my_list = MyList()
+    my_list.append(1)
+    my_list.append(4)
+    my_list.append(2)
+    my_list.append(3)
+    my_list.append(5)
+    print(my_list)
+    my_list.print_sorted()
+    print(my_list)
