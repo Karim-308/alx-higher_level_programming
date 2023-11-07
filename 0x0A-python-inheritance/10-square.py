a94e491d32aba3bbd59ed22726dadaf53dc1c361
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2023-11-07 02:26:45 +0200

    Update 10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,23 +1,47 @@
 #!/usr/bin/python3
-Rectangle = __import__('9-rectangle').Rectangle
-"""
-===================================
-module with class BaseGeometry
-===================================
-"""
+"""Module for the Square class."""
+
+
+class BaseGeometry:
+    """A class with public instance method area."""
+    def area(self):
+        """Method not implemented yet."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate value as an integer greater than 0."""
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """A class that inherits from BaseGeometry class."""
+    def __init__(self, width, height):
+        """Initialize a Rectangle instance."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        """Return a string representation of the Rectangle."""
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+    def area(self):
+        """Return the area of the Rectangle."""
+        return self.__width * self.__height
 
 
 class Square(Rectangle):
-    """Square class that inherits from Rectangle that inherits BaseGeometry"""
-
+    """A class that inherits from Rectangle class."""
     def __init__(self, size):
-        """Method for initialized the attrubutes"""
-
-        super().__init__(size, size)
+        """Initialize a Square instance."""
         self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
 
-    def area(self):
-        """rectangle area"""
-
-        return self.__size ** 2
+    def __str__(self):
+        """Return a string representation of the Square."""
+        return f"[Square] {self.__size}/{self.__size}"
