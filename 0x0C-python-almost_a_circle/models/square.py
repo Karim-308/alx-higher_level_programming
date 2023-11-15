ec921c6e485ccf25587e26a0fbac0be6e5423ddc
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2023-11-15 03:26:41 +0200

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-""" Module that contains class Square, inheritance of class Rectangle """
+""" Module that contains class Square,
+inheritance of class Rectangle
+"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """ Class Square """
+    """ Class Rectangle """
 
-    
     def __init__(self, size, x=0, y=0, id=None):
         """ Initializes instances """
         super().__init__(size, size, x, y, id)
@@ -16,9 +17,9 @@ class Square(Rectangle):
         str_square = "[Square] "
         str_id = "({}) ".format(self.id)
         str_xy = "{}/{} - ".format(self.x, self.y)
-        str_size = "{}".format(self.width)
+        str_wh = "{}/{}".format(self.width, self.height)
 
-        return str_square + str_id + str_xy + str_size
+        return str_square + str_id + str_xy + str_wh
 
     @property
     def size(self):
@@ -30,6 +31,15 @@ class Square(Rectangle):
         """ Setter size """
         self.width = value
         self.height = value
+
+    def __str__(self):
+        """ str special method """
+        str_rectangle = "[Square] "
+        str_id = "({}) ".format(self.id)
+        str_xy = "{}/{} - ".format(self.x, self.y)
+        str_size = "{}".format(self.size)
+
+        return str_rectangle + str_id + str_xy + str_size
 
     def update(self, *args, **kwargs):
         """ update method """
