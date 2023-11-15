1b81f81f4281d8c2d6d772b01d5b28bfa095b4eb
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2023-11-15 03:39:46 +0200

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,80 +10,106 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """ Initializes instances """
-        self.set_dimensions(width, height)
-        self.set_position(x, y)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
-    def set_dimensions(self, width, height):
-        """ Set dimensions """
-        self.__width = self.validate_positive_integer("width", width)
-        self.__height = self.validate_positive_integer("height", height)
+    @property
+    def width(self):
+        """ width getter """
+        return self.__width
 
-    def set_position(self, x, y):
-        """ Set position """
-        self.__x = self.validate_non_negative_integer("x", x)
-        self.__y = self.validate_non_negative_integer("y", y)
-
-    @staticmethod
-    def validate_positive_integer(attr_name, value):
-        """ Validate positive integer """
+    @width.setter
+    def width(self, value):
+        """ width setter """
         if type(value) is not int:
-            raise TypeError(f"{attr_name} must be an integer")
+            raise TypeError("width must be an integer")
         if value <= 0:
-            raise ValueError(f"{attr_name} must be > 0")
-        return value
+            raise ValueError("width must be > 0")
+        self.__width = value
 
-    @staticmethod
-    def validate_non_negative_integer(attr_name, value):
-        """ Validate non-negative integer """
+    @property
+    def height(self):
+        """ height getter """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ height setter """
         if type(value) is not int:
-            raise TypeError(f"{attr_name} must be an integer")
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """ x getter """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ x setter """
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
         if value < 0:
-            raise ValueError(f"{attr_name} must be >= 0")
-        return value
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """ y getter """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """ y setter """
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
 
     def area(self):
         """ returns the area of the rectangle object """
-        return self.__width * self.__height
+        return self.width * self.height
 
     def display(self):
         """ displays a rectangle """
-        rectangle = self.__y * "\n"
-        for _ in range(self.__height):
-            rectangle += (" " * self.__x)
-            rectangle += ("#" * self.__width) + "\n"
+        rectangle = self.y * "\n"
+        for i in range(self.height):
+            rectangle += (" " * self.x)
+            rectangle += ("#" * self.width) + "\n"
 
         print(rectangle, end='')
 
     def __str__(self):
         """ str special method """
-        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
+        str_rectangle = "[Rectangle] "
+        str_id = "({}) ".format(self.id)
+        str_xy = "{}/{} - ".format(self.x, self.y)
+        str_wh = "{}/{}".format(self.width, self.height)
+
+        return str_rectangle + str_id + str_xy + str_wh
 
     def update(self, *args, **kwargs):
         """ update method """
-        if args:
-            self.set_dimensions(args[1] if len(args) > 1 else self.__width,
-                                args[2] if len(args) > 2 else self.__height)
-            self.set_position(args[3] if len(args) > 3 else self.__x,
-                              args[4] if len(args) > 4 else self.__y)
+        if args is not None and len(args) is not 0:
+            list_atr = ['id', 'width', 'height', 'x', 'y']
+            for i in range(len(args)):
+                setattr(self, list_atr[i], args[i])
         else:
-            if 'width' in kwargs or 'height' in kwargs:
-                new_width = kwargs.get('width', self.__width)
-                new_height = kwargs.get('height', self.__height)
-                self.set_dimensions(new_width, new_height)
-            if 'x' in kwargs or 'y' in kwargs:
-                new_x = kwargs.get('x', self.__x)
-                new_y = kwargs.get('y', self.__y)
-                self.set_position(new_x, new_y)
-
-    
+            for key, value in kwargs.items():
+                setattr(self, key, value)
 
     def to_dictionary(self):
         """ method that returs a dictionary with properties """
-        return {
-            'id': self.id,
-            'width': self.__width,
-            'height': self.__height,
-            'x': self.__x,
-            'y': self.__y,
-        }
+        list_atr = ['id', 'width', 'height', 'x', 'y']
+        dict_res = {}
+
+        for key in list_atr:
+            dict_res[key] = getattr(self, key)
+
+        return dict_res
