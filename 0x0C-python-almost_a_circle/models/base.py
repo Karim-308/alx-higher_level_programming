9a8fce6867c0aee9f5c3cd4f8c08cc6e7f9b1f34
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2023-11-15 03:14:18 +0200

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,11 +1,11 @@
-# models/base.py
 class Base:
+    """Base class for shape models."""
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Initialize the base class."""
         if id is not None:
             self.id = id
         else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
-
+            type(self).__nb_objects += 1
+            self.id = type(self).__nb_objects
