28fa015b2438c28fd0b315ad53472cb628708e74
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2023-11-15 03:14:30 +0200

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,10 +1,60 @@
-#!/usr/bin/python3
-from models.base import Base
+# models/base.py
+class Base:
+    """Base class for shape models."""
+    __nb_objects = 0
 
+    def __init__(self, id=None):
+        """Initialize the base class."""
+        if id is not None:
+            self.id = id
+        else:
+            type(self).__nb_objects += 1
+            self.id = type(self).__nb_objects
+
+# models/rectangle.py
 class Rectangle(Base):
+    """Rectangle class, inherits from Base."""
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize the Rectangle."""
         super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+
+    def area(self):
+        """Return the area of the rectangle."""
+        return self.width * self.height
+
+    def display(self):
+        """Print the rectangle with '#' characters."""
+        for _ in range(self.y):
+            print()
+        for _ in range(self.height):
+            print(" " * self.x + "#" * self.width)
+
+    def __str__(self):
+        """Return a string representation of the rectangle."""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.x, self.y, self.width, self.height
+        )
+
+    def update(self, *args, **kwargs):
+        """Update the rectangle's attributes."""
+        if args:
+            attrs = ["id", "width", "height", "x", "y"]
+            for i, value in enumerate(args):
+                setattr(self, attrs[i], value)
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Return a dictionary representation of the rectangle."""
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y,
+        }
