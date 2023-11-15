906fba2c335f9ab36c4008cfa627eaad9266baea
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2023-11-15 03:38:59 +0200

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -143,7 +143,6 @@ class Rectangle(Base):
                     self.x = value
                 elif key == 'y':
                     self.y = value
-
     
     def to_dictionary(self):
         '''Returns the dictionary representation of a Rectangle.'''
