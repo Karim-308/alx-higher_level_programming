bb85d506a8bbfab2ff82bb6aa917c1287c84c9a2
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2024-03-13 02:10:17 +0200

    Create 4-first_table.sql

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+--  a script that creates a table called first_table in the current database in your MySQL server.
+-- Exceute: cat 4-first_table.sql | database_name -hlocalhost -uroot -p hbtn_0c_0
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
