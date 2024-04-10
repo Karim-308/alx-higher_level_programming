ef68a9248a9eab4171419856d8e447bf1fca1ec8
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2024-04-10 03:51:41 +0200

    Update 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,15 +1,4 @@
 #!/usr/bin/node
-const initList = require('./100-data.js').list;
-const newList = initList.map((number, index) => number * index);
-
-console.log(initList);
-console.log(newList);
-
-// Below are alternatives to newList function
-// const newList = initList.map(function (number, index) {
-//   return number * index;
-// });
-
-// const newList = initList.map((number, index) => {
-//   return number * index;
-// });
+const list = require('./100-data.js').list;
+console.log(list);
+console.log(list.map((item, index) => item * index));
