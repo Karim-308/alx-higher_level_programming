353a8dbfcfd488078c89f8e7d37f555e5c26e1c5
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2024-04-26 05:22:30 +0300

    Update 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script URL as arg, sends a GET request to URL, and displays response
-curl -sH "X-School-User-Id: 98" "$1"
+# Send a GET request to a given URL with a header variable.
+curl -sH "X-School-User-Id: 98" "${1}"
