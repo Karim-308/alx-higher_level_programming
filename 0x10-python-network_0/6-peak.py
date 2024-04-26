df6c9b26b76d155b32cd5867fe477ba434bd33f4
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2024-04-26 05:23:43 +0300

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,32 +1,24 @@
 #!/usr/bin/python3
-"""script for finding peak in list of ints, interview prep
-"""
+"""Finds a peak in a list of unsorted integers"""
 
-"""
-    THOUGHT PROCESS
-        it is not sorted, so sorting would take n(log(n))
-            -> not worth sorting
-        looping through and keeping track of max (brute force)
-            -> O(n)
-
-        possibly looping from each end reducing to 1/2 run time
-            -> still O(n)
-"""
 
 def find_peak(list_of_integers):
-    if not list_of_integers:
-        return None
-    
-    low = 0
-    high = len(list_of_integers) - 1
-    
-    while low < high:
-        mid = (low + high) // 2
-        
-        if list_of_integers[mid] > list_of_integers[mid + 1]:
-            high = mid
-        else:
-            low = mid + 1
-    
-    return list_of_integers[low]
+    """Finds a peak in list_of_integers"""
 
+    if list_of_integers is None or list_of_integers == []:
+        return None
+    lo = 0
+    hi = len(list_of_integers)
+    mid = ((hi - lo) // 2) + lo
+    mid = int(mid)
+    if hi == 1:
+        return list_of_integers[0]
+    if hi == 2:
+        return max(list_of_integers)
+    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
+            list_of_integers[mid] >= list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[mid:])
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
