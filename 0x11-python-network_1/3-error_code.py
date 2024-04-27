ad310364ec756f9ad41c1bfed5dcb3b823ce99e6
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2024-04-27 04:06:07 +0300

    Update 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-"""  sends a request to the URL and displays the body
-    of the response (decoded in utf-8).
-    """
+"""Sends a request to the URL and displays the body of the response (decoded in utf-8)."""
 
-import urllib
+import urllib.request
+import urllib.error
 import sys
-
 
 if __name__ == "__main__":
     url = sys.argv[1]
 
-try:
-    with urllib.request.urlopen(url) as response:
-        the_page = response.read()
-        print(the_page.decode('utf-8'))
-except urllib.error.URLError as e:
-    print("Error:", e)
+    try:
+        with urllib.request.urlopen(url) as response:
+            the_page = response.read()
+            print(the_page.decode('utf-8'))
+    except urllib.error.URLError as e:
+        print("Error:", e)
