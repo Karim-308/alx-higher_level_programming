a77b95c88d656fa644b1f2fe6abfc19f963a299c
Karim-308 <2001118@eng.asu.edu.eg>
2024-04-26 18:06:52 -0700

    ID

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Sends a request to the URL and displays the body of the response (decoded in utf-8)."""
+""" Sends a request to the URL and displays
+    the body of the response (decoded in utf-8).
+    """
 
 import urllib.request
 import urllib.error
