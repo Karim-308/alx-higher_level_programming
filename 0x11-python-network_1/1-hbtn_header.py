834ccb5dd0258acccf37bc8b45265ab4fecabea2
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2024-04-27 03:43:15 +0300

    Update 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Sends a req & displays the value of the X-Request-Id variable found in the header"""
+""" Sends a req & displays the value of the X-Request-Id
+    variable found in the header
+"""
 
 from urllib import request
 import sys
