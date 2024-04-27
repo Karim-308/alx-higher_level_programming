e77f23bfb3be4dcd6c89acc171d5c213787ebef5
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2024-04-27 05:37:45 +0300

    Create 8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Sends a POST request to http://0.0.0.0:5000/search_user
+with a letter as a parameter.
+Displays the id and name if the 
+response is properly JSON formatted and not empty.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    q = sys.argv[1]
+
+    info = {'q': q}
+    response = requests.post("http://0.0.0.0:5000/search_user", data=info)
+
+    try:
+        if response.status_code == 200:
+            json_response = response.json()
+            if isinstance(json_response, dict) and json_response:
+                print("[{}] {}".format(json_response['id'], json_response['name']))
+            else:
+                print("No result")
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
