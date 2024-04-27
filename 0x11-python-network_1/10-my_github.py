a666975e72a95b36e747dc93103a822463e55371
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2024-04-27 05:47:45 +0300

    Create 10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Python script that takes Github credentials (username and password)
+and uses the Github API to display the user's id
+"""
+
+import requests
+from requests.auth import HTTPBasicAuth
+import sys
+
+if __name__ == '__main__':
+    # Construct the URL for the Github API endpoint
+    github_username = sys.argv[1]
+    github_password = sys.argv[2]
+    github_api_url = 'https://api.github.com/users/{}'.format(github_username)
+
+    # Send a GET request to the Github API with basic authentication
+    response = requests.get(github_api_url,
+                            auth=HTTPBasicAuth(github_username, github_password))
+
+    # Print the user's id if the request is successful
+    if response.status_code == 200:
+        user_id = response.json().get('id')
+        print(user_id)
+    else:
+        print("Failed to fetch user id. Response status code:", response.status_code)
