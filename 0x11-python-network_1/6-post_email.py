6561490d9113445c8dbc3fc9515e38844c750dd2
Karim-308 <2001118@eng.asu.edu.eg>
2024-04-26 19:27:17 -0700

    exe

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    URL and an email address, sends a POST request to the
+    passed URL with the email as
+    a parameter, and finally displays the body of the response.
+    """
+import requests
+import sys
+
+if __name__ == '__main__':
+#write email as data input
+    info = {'email': sys.argv[2]}
+    r = requests.post(sys.argv[1], data=info)
+    print(r)
