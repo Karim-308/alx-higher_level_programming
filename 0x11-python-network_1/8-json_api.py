176ece83448d6b23ff74985a2a892e1878cec9e0
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2024-04-27 05:42:20 +0300

    Update 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
 """
-Sends a POST request to 
-http://0.0.0.0:5000/search_user
-with a letter as a parameter.
-Displays the id and name if the 
-response is properly JSON formatted and not empty.
+Sends a POST request to http://0.0.0.0:5000/search_user with a letter as a parameter.
+Displays the id and name if the response is properly JSON formatted and not empty.
 """
 
 import requests
 import sys
 
 if __name__ == "__main__":
-
     q = sys.argv[1]
 
     info = {'q': q}
-    response = requests.post("http://0.0.0.0:5000/search_user"
-                             , data=info)
+    response = requests.post("http://0.0.0.0:5000/search_user", data=info)
 
     try:
         if response.status_code == 200:
