4dbd967f28ccfda458a20dc4f8924274f1e1a58b
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2024-04-27 05:52:01 +0300

    Create 100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Python script that takes 2 arguments to solve a challenge.
+"""
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    # Construct the URL for fetching commits from a GitHub repository
+    repository_owner = argv[2]
+    repository_name = argv[1]
+    url = "https://api.github.com/repos/{}/{}/commits".format(repository_owner, repository_name)
+
+    # Send a GET request to fetch commits from the GitHub API
+    response = requests.get(url)
+
+    # Parse the response as JSON
+    commits = response.json()
+
+    # Print details of the first 10 commits
+    for commit in commits[:10]:
+        sha = commit.get('sha')
+        author_name = commit.get('commit').get('author').get('name')
+        print("{}: {}".format(sha, author_name))
