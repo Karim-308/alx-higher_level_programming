d0eeb71947cb82c06f16290327e2863fda6ced95
Karim-308 <2001118@eng.asu.edu.eg>
2024-04-26 17:16:33 -0700

    exe

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-""" Fetch data from https://alx-intranet.hbtn.io/status"""
-
+"""Fetch data from https://alx-intranet.hbtn.io/status"""
 
 import urllib.request
 
-req = urllib.request.Request('http://python.org/')
-with urllib.request.urlopen(req) as response:
-   the_page = response.read()
+url = 'https://alx-intranet.hbtn.io/status'
+with urllib.request.urlopen(url) as response:
+    the_page = response.read()
 
-    print("Body response:")
-    print("\t- type:", type(body))
-    print("\t- content:", body)
-    print("\t- utf8 content:", body.decode('utf-8'))
+print("Body response:")
+print("\t- type:", type(the_page))
+print("\t- content:", the_page)
+print("\t- utf8 content:", the_page.decode('utf-8'))
